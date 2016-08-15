dfad7643463f5f967777750b73b13ac41e8e3400
dryadb11781 <dryadb11782@gmail.com>
2016-08-15 19:00:47 +0800

    欄位個別輸出表單與客製化表單
    欄位個別輸出表單與客製化表單,超連結回首頁

--- mysite/restaurants/forms.py
+++ mysite/restaurants/forms.py
@@ -7,5 +7,14 @@ class CommentForm(forms.Form):
     """Comment Form class clear"""
 
     visitor = forms.CharField(max_length=20)
-    email = forms.EmailField(max_length=20, required=False)
+    email = forms.EmailField(max_length=20, required=False,label='E-mail')
     content = forms.CharField(max_length=200, widget=forms.Textarea())
+
+    def clean_content(self):
+        """check content if len > 5
+        :returns: cleaned content
+        """
+        content = self.cleaned_data.get('content', '')
+        if len(content) < 5:
+            raise forms.ValidationError('字數不足')
+        return content
